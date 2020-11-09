7b2629db5d621a75521897467537c97b693025a6
Mikhail Bolotov <mbolotov@devexperts.com>
2020-11-09 11:42:48 +0300

    added Array.prototype.map (fix for #1)

--- index.js
+++ index.js
@@ -4,6 +4,39 @@ var Tree = require('./lib/cypressIntellijTree')
   , stringifier = require('./lib/cypress-intellij-stringifier')
   , SingleElementQueue = require('./lib/single-element-queue');
 
+// Reference: http://es5.github.io/#x15.4.4.19
+if (!Array.prototype.map) {
+  Array.prototype.map = function(callback/*, thisArg*/) {
+    var T, A, k;
+
+    if (this == null) {
+      throw new TypeError('this is null or not defined');
+    }
+    var O = Object(this);
+    var len = O.length >>> 0;
+
+    if (typeof callback !== 'function') {
+      throw new TypeError(callback + ' is not a function');
+    }
+    if (arguments.length > 1) {
+      T = arguments[1];
+    }
+    A = new Array(len);
+    k = 0;
+
+    while (k < len) {
+      var kValue, mappedValue;
+      if (k in O) {
+        kValue = O[k];
+        mappedValue = callback.call(T, kValue, k, O);
+        A[k] = mappedValue;
+      }
+      k++;
+    }
+    return A;
+  };
+}
+
 /**
  * @param {Tree} tree
  * @param test mocha test object
